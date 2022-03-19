5f4db029fe56ccead3801e752efa26f5c674a678
Denis Radovanovic <denis@stellar-room.at>
2022-03-20 00:48:13 +0100

    Added second (failing) test scenario

--- exercise1/src/test/java/FuelTest.java
+++ exercise1/src/test/java/FuelTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -6,13 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FuelTest {
 
-    @DisplayName("Calculating Fuel Consumptions")
+    @DisplayName("Calculating Fuel Consumptions - Scenario 1")
     @ParameterizedTest(name = "{index} => mass =''{0}''")
     @ValueSource(ints = {12,14})
     void testGetFuelConsumption_Scenario1(int mass){
         Fuel fuel = new Fuel(mass);
 
         int expected = 2;
+        int actual = fuel.getFuelConsumption();
+
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Calculating Fuel Consumptions - Scenario 2")
+    @Test
+    void testGetFuelConsumption_Scenario2(){
+        Fuel fuel = new Fuel(1969);
+
+        int expected = 654;
         int actual = fuel.getFuelConsumption();
 
         assertEquals(expected, actual);
